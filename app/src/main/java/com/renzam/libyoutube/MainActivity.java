e2aadc3df26d8c367bab450577ab804f10619bd1
renji <mynameisrenjimathew@gmail.com>
2019-07-18 12:24:58 +0530

    init

--- app/src/main/java/com/renzam/libyoutube/MainActivity.java
+++ app/src/main/java/com/renzam/libyoutube/MainActivity.java
@@ -33,66 +33,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(recyclerViewAdapter);
 
 
-//        youTubePlayerView= findViewById(R.id.youtube_player_view);
-//        getLifecycle().addObserver(youTubePlayerView);
-//
-//
-//
-//        final String videoId = "P1Mq9s2LzXs";
-//        youTubePlayerView.addYouTubePlayerListener(new YouTubePlayerListener() {
-//            @Override
-//            public void onReady(YouTubePlayer youTubePlayer) {
-//
-//                youTubePlayer.loadVideo(videoId,0);
-//
-//            }
-//
-//            @Override
-//            public void onStateChange(YouTubePlayer youTubePlayer, PlayerConstants.PlayerState playerState) {
-//
-//            }
-//
-//            @Override
-//            public void onPlaybackQualityChange(YouTubePlayer youTubePlayer, PlayerConstants.PlaybackQuality playbackQuality) {
-//
-//            }
-//
-//            @Override
-//            public void onPlaybackRateChange(YouTubePlayer youTubePlayer, PlayerConstants.PlaybackRate playbackRate) {
-//
-//            }
-//
-//            @Override
-//            public void onError(YouTubePlayer youTubePlayer, PlayerConstants.PlayerError playerError) {
-//
-//            }
-//
-//            @Override
-//            public void onCurrentSecond(YouTubePlayer youTubePlayer, float v) {
-//
-//            }
-//
-//            @Override
-//            public void onVideoDuration(YouTubePlayer youTubePlayer, float v) {
-//
-//            }
-//
-//            @Override
-//            public void onVideoLoadedFraction(YouTubePlayer youTubePlayer, float v) {
-//
-//            }
-//
-//            @Override
-//            public void onVideoId(YouTubePlayer youTubePlayer, String s) {
-//
-//            }
-//
-//            @Override
-//            public void onApiChange(YouTubePlayer youTubePlayer) {
-//
-//            }
-//        });
-
 
 
 
